a6e8598daa6e16244de545109494671304ceb11c
jubao.zy <jubao.zy@antfin.com>
2021-03-03 14:09:23 +0800

    feat: init

---
+++ src/Item.js
@@ -0,0 +1,70 @@
+import React, { PureComponent } from "react";
+
+export default class Item extends PureComponent {
+  state = {
+    translateX: 10,
+    notifiedIn: false,
+    notifiedOut: false,
+  }
+
+
+  ref = React.createRef()
+  raf = null
+
+  update = () => {
+    const node = this.ref.current
+    const { onMoveout, onMovein } = this.props
+    let ined = this.state.notifiedIn
+    let outed = this.state.notifiedOut
+    const { translateX } = this.state
+    if (
+      !outed &&
+      node.getBoundingClientRect().right < 0
+    ) {
+      debugger
+      outed = true
+      onMoveout && onMoveout()
+    } else if (
+      !ined &&
+      node.getBoundingClientRect().right < window.screen.availWidth
+    ) {
+
+      ined = true
+      onMovein && onMovein()
+    }
+    // else {
+    if (outed) return
+    this.setState({
+      notifiedIn: ined,
+      notifiedOut: outed,
+      translateX: this.state.translateX - 1
+    }, () => {
+      this.raf = requestAnimationFrame(this.update)
+    })
+    // }
+
+  }
+
+  componentDidMount() {
+    this.raf = requestAnimationFrame(this.update)
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.raf);
+  }
+
+  render() {
+    const { translateX } = this.state
+    return (
+      <div ref={this.ref} style={{
+        position: 'absolute',
+        width: '150px',
+        backgroundColor: 'red',
+        flex: 'none',
+        left: window.screen.availWidth,
+        display: 'inline-block',
+        transform: `translate(${translateX}px)`,
+      }}>{this.props.item.item.text}</div>
+    )
+  }
+}
---
+++ src/Test.js
@@ -0,0 +1,79 @@
+import React, { PureComponent } from 'react';
+import Item from './Item';
+
+export default class extends PureComponent {
+  state = {
+    displayQueue: [],
+  }
+
+  data = [
+    { icon: 'a', text: '今天天气不错' },
+    { icon: 'b', text: '恭喜王大锤中奖' },
+    { icon: 'c', text: '明天会更好' },
+  ];
+
+  idx = 0;
+  raf = null;
+  containerRef = React.createRef();
+  flag = false
+
+  componentDidMount() {
+    const { displayQueue = [] } = this.state;
+    displayQueue.push({
+      item: this.data[this.idx],
+    });
+    this.setState({
+      displayQueue: [].concat(displayQueue),
+    })
+  }
+
+  onMoveout = () => {
+    const { displayQueue } = this.state
+    displayQueue.shift()
+    this.setState({
+      displayQueue: [...displayQueue]
+    })
+  }
+
+  onMovein = () => {
+
+    const { displayQueue } = this.state
+    if (displayQueue.length > 6) return
+    this.idx = (this.idx + 1) % this.data.length;
+    displayQueue.push({
+      key: Date.now(),
+      item: this.data[this.idx]
+    })
+    this.setState({
+      displayQueue: [...displayQueue]
+    })
+  }
+
+  render() {
+    const { displayQueue = [] } = this.state;
+    return <div
+      ref={this.containerRef} style={{
+        width: '100vw',
+        overflow: 'hidden',
+        height: '100px',
+        backgroundColor: 'black',
+        display: 'flex',
+        color: '#FFF'
+      }}>
+      {displayQueue.map((item) =>
+      (
+        <Item
+          key={item.key}
+          item={item}
+          onMovein={this.onMovein}
+          onMoveout={this.onMoveout} />
+      )
+        // <div style={{
+        //   position: 'relative',
+        //   display: 'inline-block',
+        //   left: item.x,
+        // }}>{item.item.text}</div>
+      )}
+    </div>;
+  }
+}
